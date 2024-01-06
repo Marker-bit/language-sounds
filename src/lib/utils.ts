3bc6e26c68132df25d58d9cdb06497899d539646
Marker-bit <maarrrkkkk@ya.ru>
2024-01-06 21:08:04 +0300

    fix db

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { Language } from "@/types";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -6,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getDb(f: (db: IDBDatabase) => void) {
-  let openRequest = indexedDB.open("words", 2);
+  let openRequest = indexedDB.open("words", 3);
 
   openRequest.onupgradeneeded = function () {
     let db = openRequest.result;
@@ -18,6 +19,27 @@ export function getDb(f: (db: IDBDatabase) => void) {
     }
     if (!db.objectStoreNames.contains("pairs")) {
       db.createObjectStore("pairs", { keyPath: "id", autoIncrement: true });
+    } else {
+      let tsx1 = db.transaction("languages", "readwrite");
+      let store1 = tsx1.objectStore("languages");
+
+      store1.getAll().onsuccess = (event: any) => {
+        const languages = event.target.result;
+        let tsx = db.transaction("pairs", "readwrite");
+        let store = tsx.objectStore("pairs");
+
+        store.getAll().onsuccess = (event: any) => {
+          for (const pair of event.target.result) {
+            store.put({
+              id: pair.id,
+              fromLanguageId: pair.fromLanguageId,
+              toLanguageId: pair.toLanguageId,
+              fromLanguage: languages.find((l: Language) => l.id === pair.fromLanguageId),
+              toLanguage: languages.find((l: Language) => l.id === pair.toLanguageId),
+            });
+          }
+        };
+      };
     }
   };
 
@@ -27,5 +49,5 @@ export function getDb(f: (db: IDBDatabase) => void) {
 
   openRequest.onerror = function () {
     console.error("Error opening database:", openRequest.error);
-  }
+  };
 }
